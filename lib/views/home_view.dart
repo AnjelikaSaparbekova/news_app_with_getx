6916b5c9d51982a04770859f883988bf96382cda
AnjelikaSaparbekova <anjelika.saparbekova5@gmail.com>
2023-10-15 23:01:57 +0300

    news app with Getx

---
+++ lib/views/home_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_with_getx/components/news_card.dart';
+import 'package:news_app_with_getx/models/top_news.dart';
+import 'package:news_app_with_getx/services/fetch_servis.dart';
+import 'package:news_app_with_getx/theme/app_colors.dart';
+import 'package:news_app_with_getx/views/search_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  TopNews? topNews;
+
+  Future<void> fetchNews() async {
+    topNews = await TopNewsService().fetchTopNews();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.appBar,
+        title: const Text(
+          "News Aggregator",
+          style: TextStyle(
+            color: AppColors.white,
+          ),
+        ),
+      ),
+      body: topNews == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: topNews!.articles.length,
+              itemBuilder: (context, index) {
+                final item = topNews!.articles[index];
+                return NewsCard(item: item);
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.appBar,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const SearchView(),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.search,
+          color: AppColors.white,
+        ),
+      ),
+    );
+  }
+}
